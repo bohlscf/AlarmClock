5e1414a7dea6aca3cb99495befc02a1ef9518405
bohlscf <44482641+bohlscf@users.noreply.github.com>
2019-03-07 23:42:54 -0500

    Add files via upload

--- Alarm_Clock.ino
+++ Alarm_Clock.ino
@@ -2,25 +2,36 @@
 
 int button = 0; // equals 1 when it is pressed; so turns off alarm
 time_t clockTime = 0;
-time_t alarmTime = 400;
+time_t alarmTime = 500;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(10, INPUT);
   pinMode(8, INPUT);
   pinMode(3, OUTPUT);
+  
+  Serial.begin(9600);
 }
 
 void loop() {
-  clockTime++;
+  if(digitalRead(8) == HIGH){
+    clockTime = 0;
+  }
   
   if(alarmTime == clockTime){
     tone(3,300);
+    clockTime++;
   }
   else if(button == 1){
     noTone(3);
   }
+  else if(clockTime < alarmTime){
+    clockTime++;
+    delay(10);
+  }
   
   if(digitalRead(10) == HIGH){ button = 1; }
   else { button =  0;}  
- }
+    
+  Serial.println(clockTime);
+}
