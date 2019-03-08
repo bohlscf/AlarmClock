a147d53cab75b9316c48232f6053926fab3c5827
bohlscf <44482641+bohlscf@users.noreply.github.com>
2019-03-07 23:08:31 -0500

    Update Alarm_Clock.ino

--- Alarm_Clock.ino
+++ Alarm_Clock.ino
@@ -1,4 +1,3 @@
-
 #include <Time.h>  
 
 int button = 0; // equals 1 when it is pressed; so turns off alarm
@@ -17,11 +16,6 @@ void setup() {
 void loop() {
   clockTime++;
   
-  //if(digitalRead(8) == HIGH){
-    //alarmTime++;
-    //delay(100);
-  //}
-  
   if(alarmTime == clockTime){
     tone(3,300);
   }
@@ -31,22 +25,4 @@ void loop() {
   
   if(digitalRead(10) == HIGH){ button = 1; }
   else { button =  0;}  
-    
-   Serial.println(clockTime);
-    void hour();
-    //tone(3,300);
-  //Alarm.timerRepeat(5, tone(3, 300));
-  
-  
-
-
-
-
-  
-  //if(digitalRead(8) == HIGH){
-    //tone(3,300);
-  //}
-  //else{
-   // noTone(3);
-  //}
-}
+ }
